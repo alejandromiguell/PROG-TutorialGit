b7d26ff6e80f9c4b4fa44956cd8b00f2c3606fb2
alejandromiguell <usr4138@salesianos-lacuesta.net>
2025-04-25 13:16:04 +0100

    fix(Main): Cambio mensajes animales

--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ public class Main {
 public static void main(String[] args) {
     
     System.out.println("Hello, World!");
+    System.out.println(doubleNumber(5.0));
+    System.out.println("Welcome to Brawl Stars!");
+    
+
+
+
 }
 }
